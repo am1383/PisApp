615ff2aef3f7ee52eb1eb6aa7aa9e2a8906c60b5
am205379@gmail.com <am205379@gmail.com>
2025-02-12 13:55:43 +0330

    Modify Login action method

--- PisApp/Controllers/UserController.cs
+++ PisApp/Controllers/UserController.cs
@@ -1,26 +1,41 @@
 using Microsoft.AspNetCore.Mvc;
 using PisApp.API.DTOs.LoginDto;
-using PisApp.API.DTOs;
 using PisApp.API.Interface;
+using PisApp.API.DTOs;
 
 namespace PisApp.API.Controllers
 {
     [ApiController]
     [Route("api/v1")]
-
     public class UserController : ControllerBase
     {
         private readonly IUserService _userService;
 
-        public UserController(IUserService userService)
+        private readonly JwtService _jwtService;
+
+        public UserController(IUserService userService, JwtService jwtService)
         {
             _userService = userService;
+            _jwtService = jwtService;
         }
 
         [HttpPost("login")]
-        public async Task<ResponseDTO<> Login(LoginDto loginDto)
+        public async Task<ResponseDto<string>> UserLogin(LoginDto loginDto)
         {
+            try 
+            {
+                var phoneNumber = loginDto.phone_number;
 
+                var userId = await _userService.FindUserIdByPhoneNumber(phoneNumber);
+
+                var token = _jwtService.GenerateToken(userId);
+
+                return new ResponseDto<string>(true, token, null, null);  
+            } 
+            catch (Exception e)
+            {
+                return new ResponseDto<string>(false, null, $"Exception : {e.Message}", null);
+            }
         }
     }
 }
