b92f62b876bd93bfcaf440e61d94ee0ed32dcf2c
am205379@gmail.com <am205379@gmail.com>
2025-02-13 16:01:53 +0330

    Implement DiscountRepository

---
+++ PisApp/Interfaces/IDiscountRepository.cs
@@ -0,0 +1,11 @@
+using PisApp.API.Entities;
+
+namespace PisApp.API.Interfaces
+{
+    public interface IDiscountRepository
+    {
+        public Task<List<PrivateDiscount>> GetPrivateDiscountCodesWithLessThanOneWeekLeft(int userId);
+        
+        public Task<int> GetGiftedDiscountCodesCount(int userId);
+    }
+}
---
+++ PisApp/Repositories/DiscountRepository.cs
@@ -0,0 +1,51 @@
+using Microsoft.EntityFrameworkCore;
+using PisApp.API.DbContextes;
+using PisApp.API.Entities;
+using PisApp.API.Interfaces;
+
+namespace PisApp.API.Repositories
+{
+    public class DiscountRepository : IDiscountRepository
+    {
+        private readonly PisAppDbContext _context;
+
+        public DiscountRepository(PisAppDbContext context)
+        {   
+            _context = context;
+        }
+
+        public async Task<List<PrivateDiscount>> GetPrivateDiscountCodesWithLessThanOneWeekLeft(int userId)
+        {
+            var query = @"
+                SELECT pc.code  
+                FROM private_code pc
+                JOIN discount_code dc ON pc.code = dc.code
+                WHERE pc.client_id = @p0
+                AND dc.expiration_time <= NOW() + INTERVAL '7 days'
+            ";
+
+            var result = await _context.Set<PrivateDiscount>()
+                                        .FromSqlRaw(query, userId) 
+                                        .ToListAsync();
+
+            return result;
+        }
+
+        public async Task<int> GetGiftedDiscountCodesCount(int userId)
+        {
+            var query = @"
+                SELECT COUNT(*) 
+                FROM private_code pc
+                JOIN discount_code dc ON pc.code = dc.code
+                WHERE pc.client_id = @p0
+                AND dc.code_type = 'private'
+                AND dc.expiration_time > NOW()";
+
+            var result = await _context.Set<Discount>()
+                                        .FromSqlRaw(query, userId)
+                                        .FirstOrDefaultAsync();
+
+            return result.count;
+        }
+    }
+}
