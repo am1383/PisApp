237cbec522eec476c7a8d2bb7801becc8aad0790
am205379@gmail.com <am205379@gmail.com>
2025-02-13 16:04:44 +0330

    Remove data annotation from user entity

--- PisApp/Entities/User.cs
+++ PisApp/Entities/User.cs
@@ -1,10 +1,7 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace PisApp.API.Entities.Common
 {
     public class User
     {
-        [Key]
         public required int client_id { get; set; }
     }
 }
