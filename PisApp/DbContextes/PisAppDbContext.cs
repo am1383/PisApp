c1f6ddd2ac0e3b8479863fbf716b50930a3dec9c
am205379@gmail.com <am205379@gmail.com>
2025-02-13 16:03:06 +0330

    Append DbSet to PisAppDbContext

--- PisApp/DbContextes/PisAppDbContext.cs
+++ PisApp/DbContextes/PisAppDbContext.cs
@@ -1,5 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-using PisApp.API.DTOs;
 using PisApp.API.Entities;
 using PisApp.API.Entities.Common;
 
@@ -13,16 +12,30 @@ namespace PisApp.API.DbContextes
             //
         }
 
-        public DbSet<BaseEntity> UserResults { get; set; } 
+        public DbSet<BaseEntity> baseEntities { get; set; } 
         public DbSet<VIPUser> vIPUsers { get; set; }
+        public DbSet<Discount> discounts { get; set; }
+        public DbSet<Refer> refers { get; set; }
         public DbSet<Address> addresses { get; set; }
-        public DbSet<UserDetailDto> UserDetailDtos { get; set; }
-        public DbSet<User> userIds { get; set; }
+        public DbSet<ShoppingCart> shoppingCarts { get; set; }
+        public DbSet<UserProfit> UserProfit { get; set; }
+        public DbSet<Cart> carts { get; set; }
+        public DbSet<UserDetail> userDetails { get; set; }
+        public DbSet<PrivateDiscount> privateDiscounts { get; set; }
+        public DbSet<User> users { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<VIPUser>().HasNoKey();
-            modelBuilder.Entity<Address>().HasNoKey();
+            modelBuilder.Entity<VIPUser>      ().HasNoKey();
+            modelBuilder.Entity<User>         ().HasNoKey();
+            modelBuilder.Entity<Cart>         ().HasNoKey();
+            modelBuilder.Entity<Discount>     ().HasNoKey();
+            modelBuilder.Entity<ShoppingCart> ().HasNoKey();
+            modelBuilder.Entity<Address>      ().HasNoKey();
+            modelBuilder.Entity<Refer>        ().HasNoKey();
+            modelBuilder.Entity<UserDetail>   ().HasNoKey();
+            modelBuilder.Entity<PrivateDiscount>().HasNoKey();
+            modelBuilder.Entity<UserProfit>()     .HasNoKey();
         }
     }
 }
