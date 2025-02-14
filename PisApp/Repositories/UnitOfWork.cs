7ba9a28d796a434762394caa8515a3bd454731ff
am205379@gmail.com <am205379@gmail.com>
2025-02-14 13:44:25 +0330

    Append ProductRepository to UnitOfWork

--- PisApp/Repositories/UnitOfWork.cs
+++ PisApp/Repositories/UnitOfWork.cs
@@ -11,16 +11,25 @@ namespace PisApp.API.Repositories.UnitOfWork
         private IUserRepository?         _usersRepository;
         private IAddressRepository?      _addressRepository;
         private ITransactionRepository?  _transactionRespository;
+        private IProductRepository?      _productRepository;
         private IReferRepository?        _referRepository;
         private IShoppingCartRepository? _shoppingCartRepository;
         private IDiscountRepository?     _discountRepository;
 
-        public IUserRepository Users => _usersRepository ??= new UserRepository(_context);
-        public IDiscountRepository Discounts => _discountRepository ??= new DiscountRepository(_context);
-        public IShoppingCartRepository ShoppingCarts => _shoppingCartRepository ??= new ShoppingCartRepository(_context);
-        public IReferRepository Refers => _referRepository ??= new ReferRepository(_context);
-        public IAddressRepository Addresses => _addressRepository ??= new AddressRepository(_context);
-        public ITransactionRepository Transactions => _transactionRespository ??= new TransactionRepository(_context);
+        public IUserRepository Users
+                => _usersRepository ??= new UserRepository(_context);
+        public IDiscountRepository Discounts 
+                => _discountRepository ??= new DiscountRepository(_context);
+        public IProductRepository Products 
+                => _productRepository ??= new ProductRepository(_context);
+        public IShoppingCartRepository ShoppingCarts 
+                => _shoppingCartRepository ??= new ShoppingCartRepository(_context);
+        public IReferRepository Refers              
+                => _referRepository ??= new ReferRepository(_context);
+        public IAddressRepository Addresses 
+                => _addressRepository ??= new AddressRepository(_context);
+        public ITransactionRepository Transactions
+                => _transactionRespository ??= new TransactionRepository(_context);
 
         public async Task<int> CompleteAsync()
         {
