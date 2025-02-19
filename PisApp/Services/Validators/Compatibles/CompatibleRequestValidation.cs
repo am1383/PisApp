16d3a7097ff93f6eadcf9a85ca5430d34e31d482
am205379@gmail.com <am205379@gmail.com>
2025-02-19 15:45:08 +0330

    Modify CompatibleProductsValidation

--- PisApp/Services/Validators/Compatibles/CompatibleRequestValidation.cs
+++ PisApp/Services/Validators/Compatibles/CompatibleRequestValidation.cs
@@ -10,8 +10,17 @@ namespace PisApp.API.Services.Dtos.Validators
             RuleFor(c => c.products_id)
                 .NotNull()
                 .WithMessage("{PropertyName} نمی‌تواند خالی باشد.")
+                .Must(HaveCorrectSize)
+                .WithMessage("{PropertyName} باید حداقل دو مقدار داشته باشد.")
                 .NotEmpty()
                 .WithMessage("{PropertyName} را وارد کنید.");
+        }
+
+        private bool HaveCorrectSize(List<int> productsId)
+        {
+            var minSize = 2;
+
+            return productsId != null && productsId.Count >= minSize;
         }
     }
 }
