616b143eb56e96b8ed3e15514fea0de2e43d2664
am205379@gmail.com <am205379@gmail.com>
2025-02-21 12:30:54 +0330

    Modify GetGiftedDiscount method query

--- PisApp/Repositories/DiscountRepository.cs
+++ PisApp/Repositories/DiscountRepository.cs
@@ -26,22 +26,36 @@ namespace PisApp.API.Repositories
                                            .ToListAsync();
         }
 
-        public async Task<int> GetGiftedDiscountCodesCount(int userId)
+        public async Task<int> GetGiftedDiscountCodesCount(string userRefferCode)
         {
             var query = @"
+                    WITH RECURSIVE referral_chain AS (
+                    SELECT
+                        referee_id,         
+                        referrer_id,
+                        1 AS level,         
+                        referee_id AS origin
+                    FROM refers
+                    
+                    UNION ALL
+
+                    SELECT
+                        r.referee_id,
+                        r.referrer_id,
+                        rc.level + 1 AS level,
+                        rc.origin
+                    FROM referral_chain rc
+                    JOIN refers r ON rc.referrer_id = r.referee_id
+                )
                 SELECT 
-                    COUNT(*) 
-                FROM 
-                    private_code pc
-                JOIN 
-                    discount_code dc ON pc.code = dc.code
-                WHERE
-                    pc.client_id = @p0
-                    AND dc.code_type = 'private'
-                    AND dc.expiration_time > NOW()";
+                    origin,
+                    (MAX(level) + 1) AS gifted_codes_count  
+                FROM referral_chain
+                GROUP BY origin
+                ";
 
             var result = await unitOfWork.Context.Set<Discount>()
-                                                 .FromSqlRaw(query, userId)
+                                                 .FromSqlRaw(query, userRefferCode)
                                                  .FirstOrDefaultAsync();
 
             return result.count;
