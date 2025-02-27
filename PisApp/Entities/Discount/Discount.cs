09559164535874a1d5d5e512acb2b57c152184cc
am205379@gmail.com <am205379@gmail.com>
2025-02-27 22:58:41 +0330

    Modify GetPrivateDiscountCodes method

--- PisApp/Entities/Discount/Discount.cs
+++ PisApp/Entities/Discount/Discount.cs
@@ -2,6 +2,6 @@ namespace PisApp.API.Entities
 {
     public class Discount
     {
-        public int count { get; set; }
+        public int gifted_codes_count { get; set; }
     }
 }
--- PisApp/Repositories/DiscountRepository.cs
+++ PisApp/Repositories/DiscountRepository.cs
@@ -10,16 +10,16 @@ namespace PisApp.API.Repositories
         public async Task<List<PrivateDiscount>> GetPrivateDiscountCodesWithLessThanOneWeekLeft(int userId)
         {
             var query = @"
-                SELECT 
-                    pc.code  
-                FROM 
-                    private_code pc
-                JOIN 
-                    discount_code dc ON pc.code = dc.code
-                WHERE 
-                    pc.client_id = @p0
-                    AND dc.expiration_time <= NOW() + INTERVAL '7 days'
-            ";
+                    SELECT 
+                        pc.code  
+                    FROM 
+                        private_code pc
+                    JOIN 
+                        discount_code dc ON pc.code = dc.code
+                    WHERE 
+                        pc.client_id = @p0
+                        AND dc.expiration_time <= NOW() + INTERVAL '7 days'
+                ";
 
             return await unitOfWork.Context.Set<PrivateDiscount>()
                                            .FromSqlRaw(query, userId) 
@@ -30,35 +30,30 @@ namespace PisApp.API.Repositories
         {
             var query = @"
                     WITH RECURSIVE referral_chain AS (
-                    SELECT
-                        referee_id,         
-                        referrer_id,
-                        1 AS level,         
-                        referee_id AS origin
-                    FROM refers
-                    
-                    UNION ALL
+                        SELECT
+                            referee_id,
+                            referrer_id
+                        FROM refers
+                        WHERE referrer_id = @p0
 
-                    SELECT
-                        r.referee_id,
-                        r.referrer_id,
-                        rc.level + 1 AS level,
-                        rc.origin
-                    FROM referral_chain rc
-                    JOIN refers r ON rc.referrer_id = r.referee_id
-                )
-                SELECT 
-                    origin,
-                    (MAX(level) + 1) AS gifted_codes_count  
-                FROM referral_chain
-                GROUP BY origin
+                        UNION ALL
+
+                        SELECT
+                            r.referee_id,
+                            r.referrer_id
+                        FROM referral_chain rc
+                        JOIN refers r ON rc.referee_id = r.referrer_id
+                    )
+                    SELECT 
+                        COUNT(*) AS gifted_codes_count
+                    FROM referral_chain
                 ";
 
             var result = await unitOfWork.Context.Set<Discount>()
                                                  .FromSqlRaw(query, userRefferCode)
                                                  .FirstOrDefaultAsync();
 
-            return result.count;
+            return result.gifted_codes_count;
         }
     }
 }
