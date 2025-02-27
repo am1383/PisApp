798ee3112da54ff158412c985402442f003eb919
am205379@gmail.com <am205379@gmail.com>
2025-02-28 00:03:48 +0330

    Modify GetUserProfit method query

--- PisApp/Repositories/TransactionRepository.cs
+++ PisApp/Repositories/TransactionRepository.cs
@@ -10,17 +10,18 @@ namespace PisApp.API.Repositories
         public async Task<UserProfit> GetUserProfitForVIPClients(int userId)
         {
             var query = @"
-                SELECT COALESCE(SUM(at.quantity * at.cart_price), 0) * 0.15 AS user_profit
-                    FROM issued_for AS ifr
-                    JOIN transaction AS t 
-                        ON ifr.tracking_code = t.tracking_code
-                    JOIN added_to AS at 
-                        ON at.client_id      = ifr.client_id
-                        AND at.cart_number   = ifr.cart_number
-                        AND at.locked_number = ifr.locked_number
-                    WHERE t.time_stamp      >= date_trunc('month', CURRENT_DATE)
-                        AND t.time_stamp     < date_trunc('month', CURRENT_DATE + INTERVAL '1 month')
-                        AND ifr.client_id    = @p0";
+                    SELECT COALESCE(SUM(at.cart_price), 0) * 0.15 AS user_profit
+                        FROM issued_for AS ifr
+                        JOIN transaction AS t 
+                            ON ifr.tracking_code = t.tracking_code
+                        JOIN added_to AS at 
+                            ON at.client_id      = ifr.client_id
+                            AND at.cart_number   = ifr.cart_number
+                            AND at.locked_number = ifr.locked_number
+                        WHERE t.time_stamp      >= date_trunc('month', CURRENT_DATE)
+                            AND t.time_stamp     < date_trunc('month', CURRENT_DATE + INTERVAL '1 month')
+                            AND ifr.client_id    = @p0
+                    ";
 
             return await unitOfWork.Context.Set<UserProfit>()
                                            .FromSqlRaw(query, userId)
