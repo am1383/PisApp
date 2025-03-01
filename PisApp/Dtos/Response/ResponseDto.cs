728d9d990685a145be1296c43e9c173a1503d1dc
am205379@gmail.com <am205379@gmail.com>
2025-03-01 15:00:45 +0330

    Remove isSuccess from ResponseDto

--- PisApp/Dtos/Response/ResponseDto.cs
+++ PisApp/Dtos/Response/ResponseDto.cs
@@ -2,17 +2,14 @@ namespace PisApp.API.Dtos
 {
     public class ResponseDto<T>
     {
-        public bool IsSuccess  { get; set; }
-
         public string? Message { get; set; }
 
         public T Data          { get; set; }
 
-        public ResponseDto(T data, bool isSuccess = true, string? message = "") 
+        public ResponseDto(T data, string message = "") 
         {  
-            IsSuccess = isSuccess;
             Message = message;
-            Data = data;
+            Data    = data;
         }
     }
 }
