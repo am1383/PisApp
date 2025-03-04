a9dac45e13ed27458114a7a097537b628b9ab5ad
am205379@gmail.com <am205379@gmail.com>
2025-03-04 11:52:57 +0330

    Append COALESCE to query CountUserRefferByCode method

--- PisApp/Repositories/ReferRepository.cs
+++ PisApp/Repositories/ReferRepository.cs
@@ -9,7 +9,10 @@ namespace PisApp.API.Repositories
     {
         public async Task<int> CountUserReferrerByCode(string referCode)
         {
-            var query  = "SELECT COUNT(*) FROM refers WHERE referrer_id = @p0";
+            var query  = @"
+                        SELECT COALESCE(COUNT(*), 0) AS count 
+                        FROM refers 
+                        WHERE referrer_id = @p0";
                 
             var result = await unitOfWork.Context.Set<Refer>()
                                                  .FromSqlRaw(query, referCode)
